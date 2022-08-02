38a7c8471a5c7d9c982b021dc84aaef846249838
raskhodchikoff <artlan@ukr.net>
2022-08-02 16:13:40 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+// Create markup
+
+function createGalleryMarkup(items) {
+  return items
+    .map(item => {
+      return `
+      <a class="gallery__item" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+      </a>
+    `;
+    })
+    .join('');
+}
+
+// Add markup in DOM
+
+gallery.innerHTML = createGalleryMarkup(galleryItems);
+
+// SimpleLightbox
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
